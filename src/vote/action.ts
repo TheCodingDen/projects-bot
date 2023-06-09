c822ccadd3a2c1203d10b684c68ae021c50c3f05
nullishamy <git@amyerskine.me>
2023-06-09 17:26:52 +0100

    fix: lint (please)

--- src/vote/action.ts
+++ src/vote/action.ts
@@ -440,9 +440,9 @@ function generateVoteFields (votes: Vote[]): [EmbedField, EmbedField] {
   const downvotes = votes.filter((v) => v.type === 'DOWNVOTE')
 
   const upvoteString =
-    upvotes.map((v) => `@${v.voter.user.username}`).join("\n") || "None";
+    upvotes.map((v) => `@${v.voter.user.username}`).join('\n') || 'None'
   const downvoteString =
-    downvotes.map((v) => `@${v.voter.user.username}`).join("\n") || "None";
+    downvotes.map((v) => `@${v.voter.user.username}`).join('\n') || 'None'
 
   return [
     {
