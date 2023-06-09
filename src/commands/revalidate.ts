d45f680f198a85ceb53c92efe4809c07cc8fdeb7
nullishamy <git@amyerskine.me>
2023-06-09 17:21:25 +0100

    fix: revert prettier (thx github)

--- src/commands/revalidate.ts
+++ src/commands/revalidate.ts
@@ -1,91 +1,91 @@
-import { SlashCommand, SlashCreator, CommandContext } from "slash-create";
-import { commandLog } from "../communication/interaction";
-import { privateLog } from "../communication/private";
+import { SlashCommand, SlashCreator, CommandContext } from 'slash-create'
+import { commandLog } from '../communication/interaction'
+import { privateLog } from '../communication/private'
 import {
   updateSubmissionState,
-  validatePendingSubmission,
-} from "../db/submission";
-import { isValidated } from "../types/submission";
-import { fetchSubmissionForContext } from "../utils/commands";
-import { getAssignedGuilds } from "../utils/discordUtils";
-import { createEmbed, updateMessage } from "../utils/embed";
-import { stringify } from "../utils/stringify";
+  validatePendingSubmission
+} from '../db/submission'
+import { isValidated } from '../types/submission'
+import { fetchSubmissionForContext } from '../utils/commands'
+import { getAssignedGuilds } from '../utils/discordUtils'
+import { createEmbed, updateMessage } from '../utils/embed'
+import { stringify } from '../utils/stringify'
 
 export default class RevalidateCommand extends SlashCommand {
-  constructor(creator: SlashCreator) {
+  constructor (creator: SlashCreator) {
     super(creator, {
-      name: "revalidate",
+      name: 'revalidate',
       description:
-        "Revalidate a submission, removing warnings if it is successful.",
-      guildIDs: getAssignedGuilds({ includeMain: true }),
-    });
+        'Revalidate a submission, removing warnings if it is successful.',
+      guildIDs: getAssignedGuilds({ includeMain: true })
+    })
   }
 
-  async run(ctx: CommandContext): Promise<void> {
-    const submission = await fetchSubmissionForContext(ctx);
+  async run (ctx: CommandContext): Promise<void> {
+    const submission = await fetchSubmissionForContext(ctx)
 
     if (!submission) {
-      return;
+      return
     }
 
     logger.debug(
       `Starting to clear warnings for ${stringify.submission(
         submission
       )} by user ${stringify.user(ctx.user)}`
-    );
+    )
 
     // Not in a pending state (validated instead)
     if (isValidated(submission)) {
-      logger.debug("Clear halting, submission in validated state");
+      logger.debug('Clear halting, submission in validated state')
       return void commandLog.warning({
-        type: "text",
-        content: "Submission has no warnings.",
-        ctx,
-      });
+        type: 'text',
+        content: 'Submission has no warnings.',
+        ctx
+      })
     }
 
     try {
-      const validated = await validatePendingSubmission(submission);
+      const validated = await validatePendingSubmission(submission)
 
-      validated.state = "PROCESSING";
-      await updateSubmissionState(submission, "PROCESSING");
-      await updateMessage(validated.submissionMessage, createEmbed(validated));
+      validated.state = 'PROCESSING'
+      await updateSubmissionState(submission, 'PROCESSING')
+      await updateMessage(validated.submissionMessage, createEmbed(validated))
 
-      logger.debug("Clear complete.");
+      logger.debug('Clear complete.')
       commandLog.info({
-        type: "text",
-        content: "Cleared warnings successfully.",
-        ctx,
-      });
+        type: 'text',
+        content: 'Cleared warnings successfully.',
+        ctx
+      })
 
       privateLog.info({
-        type: "embed",
+        type: 'embed',
         embed: {
           title: validated.name,
           description: `**${ctx.user.username}#${ctx.user.discriminator}** cleared warnings.`,
           fields: [
             {
-              name: "ID",
-              value: validated.id,
+              name: 'ID',
+              value: validated.id
             },
             {
-              name: "Source",
-              value: validated.links.source,
+              name: 'Source',
+              value: validated.links.source
             },
             {
-              name: "Author",
-              value: `<@${validated.author.id}> (@${validated.author.user.username}, ${validated.author.id})`,
-            },
-          ],
+              name: 'Author',
+              value: `<@${validated.author.id}> (@${validated.author.user.username}, ${validated.author.id})`
+            }
+          ]
         },
-        ctx: validated,
-      });
+        ctx: validated
+      })
     } catch (err) {
       commandLog.error({
-        type: "text",
-        content: "Failed to clear warnings, submission likely broken.",
-        ctx,
-      });
+        type: 'text',
+        content: 'Failed to clear warnings, submission likely broken.',
+        ctx
+      })
     }
   }
 }
--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -1,13 +1,13 @@
-import { GuildBasedChannel, User as DjsUser } from "discord.js";
-import { User as SlashCreateUser } from "slash-create";
-import { Draft } from "../types/draft";
+import { GuildBasedChannel, User as DjsUser } from 'discord.js'
+import { User as SlashCreateUser } from 'slash-create'
+import { Draft } from '../types/draft'
 import {
   AnySubmission,
   isPending,
   isRaw,
-  isValidated,
-} from "../types/submission";
-import { Vote } from "../types/vote";
+  isValidated
+} from '../types/submission'
+import { Vote } from '../types/vote'
 
 /**
  * Stringification utility functions for our object.
@@ -16,17 +16,17 @@ import { Vote } from "../types/vote";
 export const stringify = {
   submission: (submission: AnySubmission | undefined): string => {
     if (submission === undefined) {
-      return "Submission { undefined }";
+      return 'Submission { undefined }'
     }
 
-    const base = `${submission.name} (author: ${submission.authorId})}`;
+    const base = `${submission.name} (author: ${submission.authorId})}`
 
     if (isRaw(submission)) {
       // RawSubmission
-      return `Submission(RAW) { ${base} }`;
+      return `Submission(RAW) { ${base} }`
     } else if (isValidated(submission)) {
       // ValidatedSubmission
-      return `Submission(${submission.state}) { ${base} (id: ${submission.id}) (submissionMessageId: ${submission.submissionMessage.id}) (reviewThreadId: ${submission.reviewThread.id}) }`;
+      return `Submission(${submission.state}) { ${base} (id: ${submission.id}) (submissionMessageId: ${submission.submissionMessage.id}) (reviewThreadId: ${submission.reviewThread.id}) }`
     } else if (isPending(submission)) {
       // PendingSubmission
 
@@ -35,51 +35,51 @@ export const stringify = {
 
       // FIXME: Revert this to log the state directly once we can disable the matcher, which Modmail depends on
       // SEE: https://github.com/kyb3r/modmail/issues/3246
-      const loggedState = submission.state === "ERROR" ? "INVALID" : "WARNING";
+      const loggedState = submission.state === 'ERROR' ? 'INVALID' : 'WARNING'
 
-      return `Submission(${loggedState}) { ${base} }`;
+      return `Submission(${loggedState}) { ${base} }`
     } else if (
-      submission.state === "ACCEPTED" ||
-      submission.state === "DENIED"
+      submission.state === 'ACCEPTED' ||
+      submission.state === 'DENIED'
     ) {
       // CompletedSubmission
-      return `Submission(${submission.state}) { ${base} }`;
+      return `Submission(${submission.state}) { ${base} }`
     }
 
-    throw new Error(`Invalid state ${submission.state}`);
+    throw new Error(`Invalid state ${submission.state}`)
   },
 
   vote: (vote: Vote | undefined): string => {
     if (vote === undefined) {
-      return "Vote { undefined }";
+      return 'Vote { undefined }'
     }
 
-    return `Vote { (role: ${vote.role}) (type: ${vote.type}) (voter: ${vote.voter.id}) }`;
+    return `Vote { (role: ${vote.role}) (type: ${vote.type}) (voter: ${vote.voter.id}) }`
   },
 
   user: (user: DjsUser | SlashCreateUser | undefined): string => {
     if (user === undefined) {
-      return "User { undefined }";
+      return 'User { undefined }'
     }
 
-    return `User { (id: ${user.id}) (name: @${user.username}})}`;
+    return `User { (id: ${user.id}) (name: @${user.username}})}`
   },
 
   channel: (channel: GuildBasedChannel | undefined | null): string => {
     if (channel === undefined || channel === null) {
-      return "Channel { undefined }";
+      return 'Channel { undefined }'
     }
 
-    return `Channel { (id: ${channel.id}) (name: ${channel.name}) (type: ${channel.type}) (guildId: ${channel.guildId})}`;
+    return `Channel { (id: ${channel.id}) (name: ${channel.name}) (type: ${channel.type}) (guildId: ${channel.guildId})}`
   },
 
   draft: (draft: Draft | undefined): string => {
     if (draft === undefined) {
-      return "Draft { undefined }";
+      return 'Draft { undefined }'
     }
 
     return `Draft { (id: ${draft.id}) (author: ${
       draft.author.id
-    }) (timestamp: ${draft.timestamp.toLocaleString()})}`;
-  },
-};
+    }) (timestamp: ${draft.timestamp.toLocaleString()})}`
+  }
+}
